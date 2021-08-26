d9dff28bfb4e08f4065bc1773992ba95821db899
Paul Allcca Flores <paul.flores@upb.edu.co>
2021-08-26 20:59:33 +0000

    Point2

--- main.c
+++ main.c
@@ -2,12 +2,19 @@
 #include <stdlib.h>
 
 void funcPrint(void);
+void funcAl1(void);
 
 int main(void){
     funcPrint();    
+    funcAl1();
     return EXIT_SUCCESS;
 }
 
 void funcPrint(void){
     printf("Hello\n");
+}
+
+void funcAl1(void){
+    printf("Fun1\n");
+    printf("chao\n");
 }
